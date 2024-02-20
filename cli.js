0d4ab2d42ed692e60eab162f56e2be749e859114
samuel de jesus <samuel1de2jesus3@gmail.com>
2024-02-19 19:57:29 -0500

    added a switch casse function to work with the menu also made it possible to add new passwords to the table

--- cli.js
+++ cli.js
@@ -3,7 +3,6 @@ const PORT = process.env.PORT || 3001;
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// connects mysql to the server
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -36,4 +35,81 @@ function start() {
                 "Exit",
             ],
         })
+        .then((answer) => {
+            switch (answer.action) {
+                case 'View all passwords':
+                    viewAllPasswords();
+                    break;
+                case "search password":
+                    searchPasswords();
+                    break;
+                case "update password":
+                    changePasswords();
+                    break;
+                case "add a new password":
+                    addPasswords();
+                    break;
+                case "delete a password":
+                    deletePasswords();
+                    break;
+                case "settings":
+                    settings();
+                    break;
+                case "Exit":
+                    exit();
+                    break;
+            }
+        })
+    }
+
+    function viewAllPasswords(){
+        const query = 'SELECT * FROM Passwords'
+        connection.query(query, (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            start();
+        });
+    }
+    
+    function addPasswords() {
+        const query = "SELECT * FROM Passwords";
+        connection.query(query, (err, res) => {
+            if (err) throw err;
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        name: "account",
+                        message: "what website or application is this password for?",
+                    },
+                    {
+                        type: "input",
+                        name: "userName",
+                        message: "what is the user name or ID? (if not applicable leave blank)",
+                    },
+                    {
+                        type: "input",
+                        name: "password",
+                        message: "what is the password",
+                    },
+                ])
+                .then((answers) => {
+                    const query = "INSERT INTO Passwords SET ?";
+                    connection.query(
+                        query,
+                        {
+                            AccountName: answers.account,
+                            UserName: answers.userName,
+                            AccountPassword: answers.password
+                        },
+                        (err, res) => {
+                            if (err) throw err;
+                            console.log(
+                                `Added new password (${answers.password}) for ${answers.account} with the id of (${answers.userName})`
+                            );
+                            start();
+                        }
+                    );
+                });
+        });
     }
--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE Password_manager_cli;
 
 CREATE TABLE Passwords(
     ID INT AUTO_INCREMENT PRIMARY KEY,
-    AccountName VARCHAR(30) NOT NULL UINQUE,
+    AccountName VARCHAR(30) NOT NULL UNIQUE,
     UserName VARCHAR(30),
     AccountPassword VARCHAR(30) NOT NULL
 )
